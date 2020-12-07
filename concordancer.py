458e7ed5aea8dbd19de0923fdc70d7fe2934aa7e
liao961120 <liao961120@gmail.com>
2020-12-07 14:35:54 +0800

    fix bugs

--- concordancer.py
+++ concordancer.py
@@ -12,16 +12,35 @@ class Concordancer():
         Parameters
         ----------
         corpus : list
+            Corpus data
+
+        Notes
+        -----
+        Structure of corpus could be:
             [
                 {
-                    ...,
                     "<text_dictkey>": [
                         {"word": "<word>", "pos": "<pos>"},
                         {"word": "<word>", "pos": "<pos>"},
                         {"word": "<word>", "pos": "<pos>"},
                         ...
-                    ]
+                    ],
+                    ...
                 }
+            ]
+        
+        Or, simply a nested list:
+            [
+                [
+                    [
+                        ["<word>", "<pos>"],
+                        ["<word>", "<pos>"],
+                        ["<word>", "<pos>"],
+                        ...
+                    ]
+                ],
+                [...],  # another text
+                ...
             ]
         """
         self.corpus = corpus
@@ -29,7 +48,7 @@ class Concordancer():
         self.text_key = text_key
 
         # Detect corpus structure
-        a_token = corpus[0][text_key][0][0]
+        a_token = self.get_corp_data(doc_idx=0, sent_idx=0, tk_idx=0)
         token_struct = type(a_token)
         if (token_struct is not dict) and (token_struct is not list) and (token_struct is not str):
             raise Exception(f"Structure of token in text should be dict, list, or str, not {token_struct}")
@@ -38,7 +57,9 @@ class Concordancer():
 
         # Index corpus
         for doc_idx, doc in enumerate(corpus):
-            for sent_idx, sent in enumerate(doc[text_key]):
+            if self.text_key is not None: enum = doc[text_key]
+            else: enum = doc
+            for sent_idx, sent in enumerate(enum):
                 for tk_idx, token in enumerate(sent):
                     token = norm_token_struct(token)
                     for tag, item in token.items():
@@ -46,8 +67,12 @@ class Concordancer():
                             self.corp_idx[tag][item] = []
                         position = (doc_idx, sent_idx, tk_idx)
                         self.corp_idx[tag][item].append(position)
+                        
                         # Update corpus structure
-                        self.corpus[doc_idx][text_key][sent_idx][tk_idx] = token
+                        if self.text_key is not None:
+                            self.corpus[doc_idx][self.text_key][sent_idx][tk_idx] = token
+                        else:
+                            self.corpus[doc_idx][sent_idx][tk_idx] = token
 
     
     def kwic(self, keywords: Union[str, list], tag="word", left=5, right=5, regex=False):
@@ -76,11 +101,11 @@ class Concordancer():
 
         tk_start_idx = keyword_idx(sent_idx, tk_idx)
         tk_end_idx = tk_start_idx + tk_len
-        start_idx = min(tk_start_idx - left, 0)
-        end_idx = max(tk_end_idx + right, len(text))
+        start_idx = max(tk_start_idx - left, 0)
+        end_idx = min(tk_end_idx + right, len(text))
 
         return {
-            "left": text[start_idx:tk_idx],
+            "left": text[start_idx:tk_start_idx],
             "keyword": text[tk_start_idx:tk_end_idx],
             "right": text[tk_end_idx:end_idx]
         }
@@ -162,12 +187,18 @@ class Concordancer():
 
 
     def get_corp_data(self, doc_idx, sent_idx=None, tk_idx=None):
-        if sent_idx is None:
-            return self.corpus[doc_idx][self.text_key]
-        if tk_idx is None:
-            return self.corpus[doc_idx][self.text_key][sent_idx]
-        return self.corpus[doc_idx][self.text_key][sent_idx][tk_idx]
-
+        if self.text_key is not None:
+            if sent_idx is None:
+                return self.corpus[doc_idx][self.text_key]
+            if tk_idx is None:
+                return self.corpus[doc_idx][self.text_key][sent_idx]
+            return self.corpus[doc_idx][self.text_key][sent_idx][tk_idx]
+        else:
+            if sent_idx is None:
+                return self.corpus[doc_idx]
+            if tk_idx is None:
+                return self.corpus[doc_idx][sent_idx]
+            return self.corpus[doc_idx][sent_idx][tk_idx]
 
 
 ##################
@@ -204,11 +235,13 @@ def norm_token_struct(token):
 #############
 # Test
 #############
-import json
-corpus = []
-with open("test-data/tokenDict.jsonl") as f:
-    for l in f:
-        corpus.append(json.loads(l))
+if __name__ == "__main__":
+    
+    import json
+    corpus = []
+    with open("test-data/tokenDict.jsonl") as f:
+        for l in f:
+            corpus.append(json.loads(l))
 
-C = Concordancer(corpus)
+    C = Concordancer(corpus)
 # %%
