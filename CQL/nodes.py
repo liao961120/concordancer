5d41c404045cb6c8f7852ac12ec9d97e73b90458
liao <liao961120@gmail.com>
2020-12-10 23:58:06 +0800

    first draft of parser

---
+++ CQL/nodes.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+
+@dataclass
+class AttrNameNode:
+    value: str
+
+    def __repr__(self):
+        return f"{self.value}"
+
+@dataclass
+class AttrValueNode:
+    value: str
+
+    def __repr__(self):
+        return f"{self.value}"
+
+@dataclass
+class EmptyTokenNode:
+    value: None
+
+    def __repr__(self):
+        return "EMPTY_TOKEN"
+
+@dataclass
+class DefaultTokenNode:
+    value: any
+
+    def __repr__(self):
+        return f"(DFTattr is {self.value})"
+
+@dataclass
+class AssignAttrNode:
+    node_a: any
+    operator: any
+    node_b: any
+
+    def __repr__(self):
+        return f"({self.node_a} {self.operator} {self.node_b})"
+
+@dataclass
+class ConjoinAttrNode:
+    node_a: any
+    node_b: any
+
+    def __repr__(self):
+        return f"({self.node_a} & {self.node_b})"
+
+@dataclass
+class QuantifyNode:
+    node_a: any
+    quantifier: any
+
+    def __repr__(self):
+        return f"({self.node_a}_{self.quantifier})"
+
+@dataclass
+class LabelNode:
+    node_a: any
+    label: any
+
+    def __repr__(self):
+        return f"({self.label}:{self.node_a})"
---
+++ CQL/parser_.py
@@ -0,0 +1,140 @@
+from tokens import TokenType
+from nodes import *
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = iter(tokens)
+        self.advance()
+
+    def raise_error(self):
+        raise Exception("Syntax Error")
+
+    def advance(self):
+        try:
+            self.current_token = next(self.tokens)
+        except StopIteration:
+            self.current_token = None
+    
+    def parse(self):
+        if self.current_token is None:
+            return None
+        
+        result = self.list_wordGroups()
+
+        if self.current_token != None:
+            self.raise_error()
+        
+        return result
+    
+    def list_wordGroups(self):
+        result = []
+        while self.current_token != None:
+            groups = self.wordGroup()
+            result.append(groups)
+            self.advance()
+        
+        print(f"list_wordGroups: {result}")
+        return result
+    
+
+    def wordGroup(self):
+        token = self.current_token
+        word_group = []
+        label = None
+        quant = None
+
+        # Check whether group is labeled
+        if token.type == TokenType.TOKEN_LABEL:
+            label = token.value
+            self.advance()
+
+        # Parentheses as group
+        if self.current_token.type == TokenType.LPAREN:
+            self.advance()
+            while self.current_token != None and self.current_token.type in {TokenType.DEFAULT_TOKEN, TokenType.EMPTY_TOKEN, TokenType.TOKEN_LABEL, TokenType.ATTR_NAME, TokenType.LPAREN}:
+                if self.current_token.type == TokenType.LPAREN:
+                    result = self.wordGroup()
+                else:
+                    result = self.word()
+                word_group.append(result)
+            
+            if self.current_token.type != TokenType.RPAREN:
+                self.raise_error()
+            
+            # Check trailing quantifier
+            self.advance()
+            if self.current_token.type == TokenType.TOKEN_QUANTIFIER:
+                quant = self.current_token.value
+
+            # Add label and quantifiers to group
+            if quant != None:
+                word_group = QuantifyNode(word_group, quant) 
+            if label != None:
+                word_group = LabelNode(word_group, label)
+            
+            return word_group
+            
+        elif self.current_token.type in {TokenType.DEFAULT_TOKEN, TokenType.EMPTY_TOKEN, TokenType.ATTR_NAME}:
+            word_group = self.word()
+            if label != None:
+                word_group = LabelNode(word_group, label)
+
+            print(f"wordGroup: {word_group}")
+            return word_group
+        
+
+    def word(self):
+        quant = None
+        # Directly return DEFAULT/EMPTY_TOKEN
+        if self.current_token.type in {TokenType.DEFAULT_TOKEN, TokenType.EMPTY_TOKEN}:
+            if self.current_token.type == TokenType.DEFAULT_TOKEN:
+                word = DefaultTokenNode(self.current_token.value)
+            else:
+                word = EmptyTokenNode(self.current_token.value)
+            
+            self.advance()
+            if self.current_token.type == TokenType.TOKEN_QUANTIFIER:
+                quant = self.current_token.value
+            
+            if quant != None:
+                word = QuantifyNode(word, quant) 
+            
+            print(f"word: {word}")
+            return word
+
+        # Construct word from attributes
+        word = self.word_attrpair()
+
+        while self.current_token != None and self.current_token.type == TokenType.ATTR_AND:
+            self.advance()
+            word = ConjoinAttrNode(word, self.word_attrpair())
+        
+        # Check quantifier
+        print(f"TOKEN: {self.current_token}")
+        if self.current_token.type == TokenType.TOKEN_QUANTIFIER:
+            quant = self.current_token.value
+
+        if quant != None:
+            word = QuantifyNode(word, quant) 
+
+        
+        print(f"word: {word}")
+        return word
+    
+
+    def word_attrpair(self):
+        token = self.current_token
+
+        if token.type == TokenType.ATTR_NAME:
+            self.advance()
+            if self.current_token.type == TokenType.ATTR_RELATION:
+                operator = self.current_token.value
+                self.advance()
+                if self.current_token.type == TokenType.ATTR_VALUE:
+                    return AssignAttrNode(
+                        AttrNameNode(token.value), 
+                        operator, 
+                        AttrValueNode(self.current_token.value))
+
+        
+        self.raise_error()
