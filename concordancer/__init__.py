01c9957ac6e5cbff3260fa6323936e397ca425ec
liao961120 <liao961120@gmail.com>
2021-01-07 22:41:48 +0800

    defensive programming for network

--- concordancer/__init__.py
+++ concordancer/__init__.py
@@ -1,3 +1,7 @@
+import logging
 from concordancer.server import download_query_interface
 
-download_query_interface(force=True)
+try:
+    download_query_interface(force=True)
+except:
+    logging.warning("Skip download query interface")
