537a7ddf2d01b7a8cfe8e6b21abe80f469d48c58
liao961120 <liao961120@gmail.com>
2021-01-08 19:04:50 +0800

    new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", encoding="utf-8") as f:
       long_description = f.read().strip()
 
 setup(name='concordancer',
-      version='0.1.10',
+      version='0.1.11',
       description='Extract concordance lines from corpus with CQL',
       long_description=long_description,
       long_description_content_type='text/markdown',
